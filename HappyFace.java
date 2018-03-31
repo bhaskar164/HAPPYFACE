501955cd27ef87f38886bfc69f767805389eae6f
Himanshu <34915342+bhaskar164@users.noreply.github.com>
2018-03-31 19:45:53 +0530

    adding background

--- HappyFace.java
+++ HappyFace.java
@@ -3,6 +3,8 @@ import java.util.*;
 import processing.core.*;
 public class HappyFace extends PApplet {
 
+	private String url="E:\\HKMOTOG2\\photos\\beautiful-beaches-at-sunset-images-6.jpg";
+	private PImage bkimg;
 	public static void main(String[] args) {
 		PApplet.main("HappyFace");
 	}
@@ -13,10 +15,13 @@ public class HappyFace extends PApplet {
 	public void setup()
 	{
 		size(600,600);
+		bkimg=loadImage(url,"jpg");
 	}
 	public void draw()
 	{
 	
+		bkimg.resize(0,height);
+    	        image(bkimg,0,0);
 		fill(255,255,0);
 		ellipse(300, 300, 400, 400);
 		fill(0,0,0);
